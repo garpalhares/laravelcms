a9e2bff647e0e6c9b0b5a1fc4c08bb97617abf82
Gabriel Palhares <garpalhares@gmail.com>
2020-02-29 18:58:13 -0300

    instalação e configuiração inicial do template adminLTE

--- README.md
+++ README.md
@@ -76,3 +76,7 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+## External Libs
+[Laravel-AdminLTE](https://github.com/jeroennoten/Laravel-AdminLTE/#61-title)
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,19 @@
-<h1>Página inicial do admin</h1>
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+	<h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+	...
+@endsection
+
+@section('css')
+	<link rel="stylesheet" href="/assets/css/custom.css">
+@endsection
+
+@section('js')
+	<script>alert('teste')</script>
+@endsection
