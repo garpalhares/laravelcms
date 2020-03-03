2009dcfe8f4c493976231cfb50e741b22c5772b9
Gabriel Palhares <garpalhares@gmail.com>
2020-03-03 18:04:01 -0300

    dashboard finalizado com dados dinâmicos e gráficos implementados

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Visitor;
+use App\Page;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -12,8 +15,56 @@ class HomeController extends Controller
 		$this->middleware('auth');
 	}
 
-    public function index()
+    public function index(Request $request)
     {
-    	return view('admin.home');
+    	$visitsCount = 0;
+    	$onlineCount = 0;
+    	$pageCount = 0;
+    	$usersCount = 0;
+        $interval = intval($request->input('interval', 30));
+        ($interval > 120) ? $interval = 120 : '';
+
+    	// Contagem de Visitantes
+        $dateInterval = date('Y-m-d H:i:s', strtotime('-'. $interval .' days'));
+    	$visitsCount = Visitor::where('date_access', '>=', $dateInterval)->count();
+
+    	// Contagem de Usuários Online
+    	$dateLimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+    	$onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)->groupBy('ip')->get();
+    	$onlineCount = count($onlineList);
+
+    	// Contagem de Páginas Totais
+    	$pageCount = Page::count();
+
+    	// Contagem de usuários
+    	$usersCount = User::count();
+
+
+        // Contagem de visitas para o #pagepie
+        $pagePie = [];
+        $visitsAll = Visitor::selectRaw('page, count(page) as c')
+            ->where('date_access', '>=', $dateInterval)
+            ->groupBy('page')
+            ->get();
+
+        foreach ($visitsAll as $visit) {
+            $pagePie[ $visit['page'] ] = intval($visit['c']);
+        }
+
+    	$pageLabels = json_encode( array_keys($pagePie) );
+    	$pageValues = json_encode( array_values($pagePie) );
+
+
+    	return view('admin.home', [
+    		'visitsCount' => $visitsCount,
+    		'onlineCount' => $onlineCount,
+    		'pageCount' => $pageCount,
+    		'usersCount' => $usersCount,
+    		'pageLabels' => $pageLabels,
+    		'pageValues' => $pageValues,
+            'dateInterval' => $interval,
+    	]);
+
+
     }
 }
